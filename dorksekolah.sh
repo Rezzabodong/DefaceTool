e5e7cfdb6dccf4fca49324c9ab8c39d151ec4724
R24 <42667691+Rezzabodong@users.noreply.github.com>
2018-11-18 07:50:28 +0000

    Create dorksekolah.sh

---
+++ dorksekolah.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+red='\033[31;1m'
+green='\033[32;1m'
+clear
+echo $green"___  ____ ____ _  _    ____ ____ _  _ ____ _    ____ _  _"
+echo "|  \ |  | |__/ |_/  __ [__  |___ |_/  |  | |    |__| |__|"
+echo "|__/ |__| |  \ | \_    ___] |___ | \_ |__| |___ |  | |  |"
+echo""
+sleep 2
+echo $red"|--------------------------------|"
+echo $red"|>>>inurl:/html/siswa.php?<<<    |"
+echo "|================================|"
+echo "|>>>inurl:/html/alumni.php?<<<   |"
+echo "|================================|"
+echo "|>>>inurl:/html/guru.php?<<<     |"
+echo "|================================|"
+echo "|            Done✓               |"
+echo "|        Created By : R24        |"
+echo "|    Thanks For Using This Tool  |"
+echo "|--------------------------------|"
